fce906769460734209b055c2ed77cbe27abd94c2
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-09-30 19:43:10 +0900

    Add react-motion

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,12 +1,18 @@
 import React, { PropTypes } from 'react'
 import { formatMoney } from 'accounting'
+import { Motion, spring } from 'react-motion'
 
 export const Balance = ({ balance, balanceAdd }) => (
   <div className='mid-gray tracked-tight f1-ns f2 pa4-ns pa3'>
-    {formatMoney(balance, '₩ ', 0)}
     <button className='fr' onClick={balanceAdd(500)}>
       Add 500
     </button>
+    <Motion className='fl' defaultStyle={{ x: 0 }} style={{ x: spring(balance, { stiffneess: 210, damping: 30 }) }}>
+      {
+        val => <div key='balance'><span>₩ </span>{formatMoney(val.x, '', 0)}</div>
+      }
+    </Motion>
+
   </div>
 )
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 
 export const Cart = ({ items }) => (
-  <div className='mid-gray tracked-tight f1-ns f2 pa4-ns pa3 mv3'>
+  <div className='mid-gray tracked-tight f3-ns f4 pa4-ns pa3 mv3'>
     {items}
   </div>
 )
